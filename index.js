563cf34da930f0ffef265575c656083b587db9aa
Adhayan Sharma <96379455+adhayan002@users.noreply.github.com>
2023-10-11 16:26:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -24,9 +24,10 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Origin', 'https://your-frontend-domain.com');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.header('Access-Control-Allow-Credentials', 'true'); // Enable credentials
   next();
 });
 
